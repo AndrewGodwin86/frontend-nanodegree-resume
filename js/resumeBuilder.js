81f1d031e8a4c7437849020efadd28e9c6026909
Andrew Godwin <andrew.godwin86@gmail.com>
2015-02-10 13:38:51 +0000

    End of lesson 1 commit
    Clean resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,95 @@
-$("#main").append("Andrew Godwin");
+
+var bio = {
+	"name": "Andrew Godwin",
+	"role": "Front-end web developer",
+	"contactInfo": {
+		"email": "andrew.godwin86@gmail.com",
+		"twitter": "@AndrewGodwin86",
+		"github": "AndrewGodwin86",
+		"blog": "http://www.andrewrgodwin.com"
+	},
+	"pictureURL": "images/me.jpg",
+	"welcomeMessage": "Hi, welcome to my resume!",
+	"skills": ["Programming", "Maths", "JavaScript"]
+};
+
+var work = {
+	"jobs": [
+		{
+			"Employer": "GodwinConsulting.Net",
+			"Position": "Consultant",
+			"workDates": "2009-2014",
+			"workCity": "London",
+			"description": "Carrying out various tasks including software audits, IT service assessments, web development."
+		},
+		{
+			"employer": "Make Architects",
+			"position": "Business and Information Systems Coordinator",
+			"workDates": "2008-2015",
+			"workCity": "London",
+			"description": "Responsible for managing and developing the business systems of the practice, including Intranet, DAM system etc."
+		}
+	]
+};
+
+
+var education = {
+	"schools": [
+		{
+			"schoolName": "Newcastle University",
+			"schoolDates": "2005-2009",
+			"schoolDegree": "MMath (Hons)",
+			"schoolLocation": "Newcastle-upon-Tyne, UK",
+			"schoolMajor": ["Mathematics"]
+		}
+	],
+	"onlineCourses": [
+		{
+			"onlineSchool": "Udacity",
+			"onlineTitle": "Front end web developer Nanodegree",
+			"onlineDates": "Present"
+		}
+	]
+};
+
+var projects = {
+	"projects": [
+		{
+			"title": "Personal website",
+			"dates": "2014",
+			"description": "Design and development of my blog",
+			"images": []
+		},
+		{
+			"title": "Paper-Stories.com",
+			"dates": "2014",
+			"description": "Development and build of portfolio and blog site",
+			"images": []
+		}
+	]
+}
+
+
+
+/*var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedEmail = HTMLemail.replace("%data%", bio.contactInfo.email);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contactInfo.twitter);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contactInfo.github);
+var formattedBlog = HTMLblog.replace("%data%", bio.contactInfo.blog);
+var formattedPic = HTMLbioPic.replace("%data%", bio.pictureURL);
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedEmail);
+$("#header").append(formattedBlog);
+$("#header").append(formattedTwitter);
+$("#header").append(formattedGithub);
+$("#header").append(formattedPic);
+$("#header").append(formattedWelcomeMsg);
+$("#header").append(formattedSkills);
+*/
+
